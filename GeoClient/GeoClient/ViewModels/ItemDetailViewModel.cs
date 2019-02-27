ab32a55209fc0611fe8014b4b8d7f9cb15535e31
Robert Wittek <r.m.wittek@gmail.com>
2019-02-27 22:16:03 +0100

    #37 grey-out map button, if no location is available

--- GeoClient/GeoClient/ViewModels/ItemDetailViewModel.cs
+++ GeoClient/GeoClient/ViewModels/ItemDetailViewModel.cs
@@ -1,15 +1,37 @@
-﻿
-using GeoClient.Models;
+﻿using GeoClient.Models;
+using Xamarin.Forms;
 
 namespace GeoClient.ViewModels
 {
     public class ItemDetailViewModel : BaseViewModel
     {
+        private static readonly Color ActiveButtonColor = Color.FromHex("#B70E0C");
+        private static readonly Color DisableButtonColor = Color.Gray;
+
         public IncidentItem IncidentItem { get; set; }
+
+        public Color OpenLocationButtonColor => GetOpenLocationButtonColor();
+        public string OpenLocationButtonText => GetOpenLocationButtonText();
+
         public ItemDetailViewModel(IncidentItem incidentItem = null)
         {
             Title = incidentItem?.DescriptiveType;
             IncidentItem = incidentItem;
+        }
+
+        private Color GetOpenLocationButtonColor()
+        {
+            return IsLocationAvailable() ? ActiveButtonColor : DisableButtonColor;
+        }
+
+        private string GetOpenLocationButtonText()
+        {
+            return IsLocationAvailable() ? "Berufungsort auf Karte anzeigen" : "Berufungsort ist nicht verortet";
+        }
+
+        private bool IsLocationAvailable()
+        {
+            return IncidentItem?.Location != null;
         }
     }
 }
