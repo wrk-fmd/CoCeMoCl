775131390f04bceedf394c26cad5907598bba744
Robert Wittek <18316389+robo-w@users.noreply.github.com>
2025-04-03 11:08:09 +0200

    update to 1.2.6

--- GeoClient/GeoClient.Android/Properties/AssemblyInfo.cs
+++ GeoClient/GeoClient.Android/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WRK BT FMD")]
 [assembly: AssemblyProduct("GeoClient.Android")]
-[assembly: AssemblyCopyright("Copyright © WRK BT FMD 2019")]
+[assembly: AssemblyCopyright("Copyright © WRK BT FMD 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.2.6.0")]
+[assembly: AssemblyFileVersion("1.2.6.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
